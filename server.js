e1da54886f6f8541cb9a910cf9d582053725f5de
kdoh52 <kdoh52@gmail.com>
2021-01-15 13:24:58 -0800

    initialize files, server connect w/ atlas

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Established connection with MongoDB database.')
+});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
