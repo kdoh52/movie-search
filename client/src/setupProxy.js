87c743e5095a939e110042297ea31daed88b0024
kdoh52 <kdoh52@gmail.com>
2021-01-19 15:44:27 -0800

    takeout http-proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(createProxyMiddleware("/api", { target: "https://shoppies-movie-search.herokuapp.com/" }));
-//   console.log('hi')
+//   app.use(createProxyMiddleware("/api", { target: "http://localhost:3001/" }));
+  console.log('hi')
 };
