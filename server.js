a7c45fc737fb1928ab3ba0fbcafc7b9ec185ba6d
kdoh52 <kdoh52@gmail.com>
2021-01-18 22:55:01 -0800

    body-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const mongoose = require("mongoose");
@@ -8,9 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
