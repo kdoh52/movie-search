a8d5f67f4ebe9c501ef7e78556b1506281428dad
kdoh52 <kdoh52@gmail.com>
2021-01-19 15:00:02 -0800

    small updates

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(routes);
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
