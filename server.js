6cf6176425c575ebc634704bf0a9fcb37ee61ff2
kdoh52 <kdoh52@gmail.com>
2021-01-18 23:02:12 -0800

    move app.get

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 // Add routes, both API and view
 app.use(routes);
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 // Connect to the Mongo DB
 const uri = process.env.ATLAS_URI;
 
